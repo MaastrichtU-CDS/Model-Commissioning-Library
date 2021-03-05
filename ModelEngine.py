f130886ffca185137127a4abe8b4d5c1ad1f671b
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2021-03-05 22:43:31 +0100

    Fetching model input parameters

--- ModelEngine.py
+++ ModelEngine.py
@@ -1,29 +1,54 @@
 import rdflib
 import os
+from string import Template
 
 class FML:
     prefix = "https://fairmodels.org/ontology.owl#"
     logisticRegression = prefix + "Logistic_Regression"
 
+class LogisticRegression:
+    def __init__(self, modelUri, modelEngine):
+        self.__modelEngine = modelEngine
+        self.__modelUri = modelUri
+    def executeModel(self):
+        modelParameters = self.getModelParameters()
+        print(modelParameters)
+    def getModelParameters(self):
+        queryResults = self.__modelEngine.performQueryFromFile(queryName="linearParams", mappings={"modelUri": self.__modelUri})
+        output = dict()
+        for row in queryResults:
+            output[str(row["inputFeature"])] = {
+                "featureName": str(row["inputFeatureName"]),
+                "beta": float(str(row["beta"]))
+            }
+        return output
+
 class ModelEngine:
     def __init__(self, modelUri):
         self.__graph = rdflib.Graph()
         self.__graph.parse(modelUri, format=rdflib.util.guess_format(modelUri))
-    
-    def getSparqlQueryFromFile(self, queryName):
+    def test(self):
+        print("hi there!")
+    def __getSparqlQueryFromFile(self, queryName, mappings=None):
         query = ""
         with open(os.path.join("queries", queryName + ".sparql")) as f:
             query = f.read()
+        if mappings is not None:
+            temp_obj = Template(query)
+            query = temp_obj.substitute(**mappings)
         return query
 
+    def performQueryFromFile(self, queryName, mappings=None):
+        query = self.__getSparqlQueryFromFile(queryName=queryName, mappings=mappings)
+        return self.__graph.query(query)
+    
     def getModelExecutor(self):
-        query = self.getSparqlQueryFromFile("modelType")
-        queryResults = self.__graph.query(query)
+        queryResults = self.performQueryFromFile("modelType")
         
         for resultRow in queryResults:
             algorithmTypeString = str(resultRow["algorithmType"])
 
             if FML.logisticRegression == algorithmTypeString:
-                return "LogReg"
+                return LogisticRegression(str(resultRow["algorithm"]), self)
         
         return None
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from ModelEngine import ModelEngine
 
 modelEngine = ModelEngine("https://fairmodels.org/models/radiotherapy/stiphout_2011.ttl")
-print(modelEngine.getModelExecutor())
+modelExecutor = modelEngine.getModelExecutor()
+modelExecutor.executeModel()
