01f4f0946dc3b4408cc261efbeb74083beca3aaf
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2021-03-05 23:21:15 +0100

    Execution example

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ from ModelEngine import ModelEngine
 
 modelEngine = ModelEngine("https://fairmodels.org/models/radiotherapy/stiphout_2011.ttl")
 modelExecutor = modelEngine.getModelExecutor()
-modelExecutor.executeModel(inputValues={
+probability = modelExecutor.executeModel(inputValues={
     "https://fairmodels.org/models/radiotherapy/#InputFeature_cTStage": 3,
-    "https://fairmodels.org/models/radiotherapy/#InputFeature_cNStage": 0,
-    "https://fairmodels.org/models/radiotherapy/#InputFeature_TLength": 2.5
+    "https://fairmodels.org/models/radiotherapy/#InputFeature_cNStage": 1,
+    "https://fairmodels.org/models/radiotherapy/#InputFeature_TLength": 15
 })
+print(probability)
