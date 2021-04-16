0c99a7e7a7f3426de62077d9c6639261e8a04dbe
Johan van Soest <info@johanvansoest.nl>
2021-04-16 14:59:27 +0200

    Added test example dataset

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from ModelEngine import ModelEngine
+import pandas as pd
 
 modelEngine = ModelEngine("https://fairmodels.org/models/radiotherapy/stiphout_2011.ttl")
 modelExecutor = modelEngine.getModelExecutor()
@@ -8,3 +9,10 @@ probability = modelExecutor.executeModel(inputValues={
     "https://fairmodels.org/models/radiotherapy/#InputFeature_TLength": 15
 })
 print(probability)
+
+inputData = pd.DataFrame(data={
+    "identifier": [1, 2, 3, 4, 5],
+    "cT": [3, 2, 3, 4],
+    "cN": [1, 0, 2, 1],
+    "tLength": [15, 4, 7, 10] 
+})
