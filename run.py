f3feaeb953a92783a64900290076f6679fe19779
Johan van Soest <info@johanvansoest.nl>
2021-05-11 13:25:51 +0200

    Including tests to see if terminology values still give the same results

--- run.py
+++ run.py
@@ -10,11 +10,30 @@ probability = modelExecutor.executeModel(inputValues={
 })
 print(probability)
 
+probability = modelExecutor.executeModel(inputValues={
+    "https://fairmodels.org/models/radiotherapy/#InputFeature_cTStage": "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48728",
+    "https://fairmodels.org/models/radiotherapy/#InputFeature_cNStage": 1,
+    "https://fairmodels.org/models/radiotherapy/#InputFeature_TLength": 15
+})
+print(probability)
+
 inputData = pd.DataFrame(data={
     "identifier": [1, 2, 3, 4,],
     "cT": [3, 2, 3, 4],
     "cN": [1, 0, 2, 1],
     "tLength": [15, 4, 7, 10] 
 })
+print(modelExecutor.executeModelOnDataFrame(inputData))
 
+inputData = pd.DataFrame(data={
+    "identifier": [1, 2, 3, 4,],
+    "cT": [
+        "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48728", 
+        "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48724", 
+        "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48728", 
+        "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48732"
+    ],
+    "cN": [1, 0, 2, 1],
+    "tLength": [15, 4, 7, 10] 
+})
 print(modelExecutor.executeModelOnDataFrame(inputData))
